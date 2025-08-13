95972d9120ba29277b7cf6fd607686462fc31912
unknown <puneetsingh91100@gmail.com>
2025-08-13 22:37:39 +0530

    4th commit

--- controllers/collegeController.js
+++ controllers/collegeController.js
@@ -1,14 +1,35 @@
 const College = require("../models/College");
 
+// exports.getColleges = async (req, res) => {
+//   const colleges = await College.find();
+//   res.json(colleges);
+// };
+
+// // For seeding colleges (can remove later)
+// exports.addCollege = async (req, res) => {
+//   const { name, domain } = req.body;
+//   const college = new College({ name, domain });
+//   await college.save();
+//   res.json(college);
+// };
 exports.getColleges = async (req, res) => {
-  const colleges = await College.find();
-  res.json(colleges);
+  try {
+    const colleges = await College.find({});
+    res.json(colleges); // Make sure this is a plain array!
+  } catch (error) {
+    console.error("Error fetching colleges:", error);
+    res.status(500).json({ msg: "Server error fetching colleges" });
+  }
 };
 
-// For seeding colleges (can remove later)
 exports.addCollege = async (req, res) => {
-  const { name, domain } = req.body;
-  const college = new College({ name, domain });
-  await college.save();
-  res.json(college);
+  try {
+    const { name, domain } = req.body;
+    const college = new College({ name, domain });
+    await college.save();
+    res.status(201).json(college);
+  } catch (error) {
+    console.error("Error adding college:", error);
+    res.status(500).json({ msg: "Server error adding college" });
+  }
 };
