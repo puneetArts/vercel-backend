c2a2d613403bc538e252d4e0e1e15839f3771a5a
unknown <puneetsingh91100@gmail.com>
2025-08-18 12:39:42 +0530

    cv-newest commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const collegeRoutes = require('./routes/colleges');
 app.use(cors({
   origin: [
     'http://localhost:5173', // local dev
-    'https://vercel-frontend-ltqb.vercel.app' // deployed frontend https://vercel-frontend-ltqb.vercel.app/
+    'https://vercel-frontend-wmdy.vercel.app' // deployed frontend 
   ],
   credentials: true
 }));
