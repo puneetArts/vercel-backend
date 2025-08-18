430436405bce38595ef34bf1f5f559769afe6b49
unknown <puneetsingh91100@gmail.com>
2025-08-18 14:26:40 +0530

    newest commit9

--- models/Resume.js
+++ models/Resume.js
@@ -4,8 +4,8 @@ const resumeSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     college: { type: mongoose.Schema.Types.ObjectId, ref: "College", required: true },
-    resumeUrl: { type: String, required: true }, // Cloudinary URL
-    originalName: { type: String }, // store original file name (optional)
+    fileUrl: { type: String, required: true }, // Cloudinary URL
+    fileName: { type: String }, // store original file name (optional)
   },
   { timestamps: true }
 );
