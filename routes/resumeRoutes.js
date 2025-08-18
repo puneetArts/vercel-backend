7d201d317b9e4e4806beaa7bacc2d62b30ef5fe9
unknown <puneetsingh91100@gmail.com>
2025-08-18 15:56:39 +0530

    rescss commit9

--- routes/resumeRoutes.js
+++ routes/resumeRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const resumeUpload = require("../middlewares/resumeUpload");
-const { uploadResume, getUserResumes, getCollegeResumes } = require("../controllers/resumeController");
+const { uploadResume, getUserResume, getCollegeResumes } = require("../controllers/resumeController");
 const auth = require('../middlewares/authMiddleware');
 
 // Upload resume
 router.post("/", auth, resumeUpload.single("resume"), uploadResume);
 
 // Get user resumes
-router.get("/user/:userId", auth, resumeController.getUserResume);
+router.get("/user/:userId", auth, getUserResume);
 
 // Get college resumes
 router.get("/college", auth, getCollegeResumes);
